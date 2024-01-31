6e1917bbc42a54befd6b4922e1aab9b70c7fcdc9
Andrea <gigli.andrea91@gmail.com>
2024-01-31 13:38:31 +0100

    feat: Allow train-val-test split for sklearn methods
    This is achieved concatenating train and val sets and using a custom sklearn data splitter.
    
    Moreover, constraints on parsed arguments have been relaxed to even enable preliminary split data. That is, data that is already stored and loaded as train-(val)-test.
    
    Most arguments relating to function calls can now be omitted as _passthrough functions have been defined and used by default.

--- src/data/preprocess_exampledb.py
+++
@@ -1,8 +0,0 @@
-from typing import Tuple
-
-from pandas import DataFrame
-
-
-def preprocess_data(X: DataFrame, Y: DataFrame) -> Tuple[DataFrame, DataFrame]:
-    """Takes dataframes X and Y and returns two dataframes X and Y."""
-    return X, Y
